fcbc9d56ebc430949b8c8e61b61f2a50831d34ae
Dang Minh Duc <dfmcvn@icloud.com>
2024-11-02 15:12:18 +0700

    initialize base template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import DemoComponent from './components/DemoComponent.vue';
 
 </script>
