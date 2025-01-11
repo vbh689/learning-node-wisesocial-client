9c05f8816a33d597ded78451a728a3f159d5902b
Dang Minh Duc <dfmcvn@icloud.com>
2025-01-11 17:13:18 +0700

    friend suggest function

--- src/components/SuggestionComponent.vue
+++ src/components/SuggestionComponent.vue
@@ -5,53 +5,20 @@
     </div>
     <!--sd-title end-->
     <div class="suggestions-list">
-      <div class="suggestion-usd">
-        <img src="../assets/images/resources/s1.png" alt="" />
+      <div v-for="friend in suggestFriend" class="suggestion-usd">
+        <img
+          v-if="friend.avatar == null"
+          src="../assets/images/resources/s1.png"
+          alt=""
+        />
+        <img v-else :src="friend.avatar" alt="" />
         <div class="sgt-text">
-          <h4>香野綾花</h4>
-          <span>Graphic Designer</span>
+          <h4>{{ friend.name }}</h4>
+          <span>{{ friend.experience }}</span>
         </div>
-        <span><i class="fa fa-plus"></i></span>
-      </div>
-      <div class="suggestion-usd">
-        <img src="../assets/images/resources/s2.png" alt="" />
-        <div class="sgt-text">
-          <h4>安座間麗美</h4>
-          <span>PHP Developer</span>
-        </div>
-        <span><i class="fa fa-plus"></i></span>
-      </div>
-      <div class="suggestion-usd">
-        <img src="../assets/images/resources/s3.png" alt="" />
-        <div class="sgt-text">
-          <h4>寺田航平</h4>
-          <span>Wordpress Developer</span>
-        </div>
-        <span><i class="fa fa-plus"></i></span>
-      </div>
-      <div class="suggestion-usd">
-        <img src="../assets/images/resources/s4.png" alt="" />
-        <div class="sgt-text">
-          <h4>長谷部千佳</h4>
-          <span>C & C++ Developer</span>
-        </div>
-        <span><i class="fa fa-plus"></i></span>
-      </div>
-      <div class="suggestion-usd">
-        <img src="../assets/images/resources/s5.png" alt="" />
-        <div class="sgt-text">
-          <h4>上田梓</h4>
-          <span>Graphic Designer</span>
-        </div>
-        <span><i class="fa fa-plus"></i></span>
-      </div>
-      <div class="suggestion-usd">
-        <img src="../assets/images/resources/s6.png" alt="" />
-        <div class="sgt-text">
-          <h4>システム太郎</h4>
-          <span>PHP Developer</span>
-        </div>
-        <span><i class="fa fa-plus"></i></span>
+        <span v-on:click="addFriend(friend.id)"
+          ><i class="fa fa-plus"></i
+        ></span>
       </div>
     </div>
     <!--suggestions-list end-->
@@ -60,7 +27,7 @@
 
 <script>
 // import Vue from 'vue'
-import axios from 'axios'
+import axios from "axios";
 
 // import component1 from 'component1'
 // import component2 from 'component2'
@@ -77,17 +44,16 @@ export default {
      ******************************* Initialize global variables ***********************************************
      **********************************************************************************************************/
     return {
-      msg: 'Hello world!',
+      msg: "Hello world!",
       users: [],
       token: sessionStorage.getItem("token"),
-      suggestFriend: []
-    }
+      suggestFriend: [],
+    };
   },
   created() {
     /***********************************************************************************************************
      *********************** Initialize data when this component is used. **************************************
      **********************************************************************************************************/
-    console.log('Init created component and call to function get data from api server.');
     this.listSuggestFriend();
   },
   mounted() {
@@ -100,13 +66,18 @@ export default {
      ********************************* Methods change value for a variable *************************************
      **********************************************************************************************************/
     msg() {
-      console.log("When the value of the msg variable changes, this method will be executed.");
-    }
+      console.log(
+        "When the value of the msg variable changes, this method will be executed."
+      );
+    },
   },
   computed: {
     appendMsg() {
-      return msg + "Process the value and assign the value to the corresponding variable the var has changed.";
-    }
+      return (
+        msg +
+        "Process the value and assign the value to the corresponding variable the var has changed."
+      );
+    },
   },
   methods: {
     /***********************************************************************************************************
@@ -121,7 +92,7 @@ export default {
     },
 
     /**
-     * Example default function using param 
+     * Example default function using param
      *
      * @param int pageNum number of page
      * @return boolean
@@ -135,17 +106,45 @@ export default {
      ******* Async and await functions for manipulating server-side data through internal API protocols ********
      **********************************************************************************************************/
 
+    async addFriend(friendId) {
+      try {
+        const callAPI = await axios.get(
+          "http://localhost/wisesocial_api/public/api/add-friend",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + this.token,
+            },
+            params: {
+              friend_id: friendId
+            }
+          }
+        );
+        if (callAPI.data.code == 200) {
+          this.listSuggestFriend();
+        } else {
+          alert("Call API failed, please check again!");
+        }
+        console.log(callAPI.data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+
     /**
      * Call API sample
      */
     async listSuggestFriend() {
       try {
-        const callAPI = await axios.get('http://localhost/wisesocial_api/public/api/list-suggest-friend', {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + this.token,
+        const callAPI = await axios.get(
+          "http://localhost/wisesocial_api/public/api/list-suggest-friend",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + this.token,
+            },
           }
-        });
+        );
         if (callAPI.data.code == 200) {
           this.suggestFriend = callAPI.data.data;
         } else {
@@ -159,20 +158,23 @@ export default {
 
     async deleteUser(id) {
       try {
-        const callAPI = await axios.delete('http://localhost/DemoAPI/public/api/delete-user/' + id, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer 12|KpRlUny9IfRhG9h8wp3Emi7Mwv1eR1d5dt06Wavkb1be01bc'
+        const callAPI = await axios.delete(
+          "http://localhost/DemoAPI/public/api/delete-user/" + id,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization:
+                "Bearer 12|KpRlUny9IfRhG9h8wp3Emi7Mwv1eR1d5dt06Wavkb1be01bc",
+            },
           }
-        }); 
+        );
         this.callAPI();
       } catch (err) {
         console.log(err);
       }
-
-    }
+    },
   },
-}
+};
 </script>
 
 <style>
