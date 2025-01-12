a7996f66aeaf59f761d02f6f697d6840a5f0ffb8
Dang Minh Duc <dfmcvn@icloud.com>
2025-01-12 16:10:03 +0700

    most followed card

--- src/components/MostFollowComponent.vue
+++ src/components/MostFollowComponent.vue
@@ -13,49 +13,136 @@
         <div class="carousel-inner">
           <div class="carousel-item active">
             <div class="user-profy">
-              <img src="../assets/images/resources/user2.png" alt="" />
-              <h3>John Doe</h3>
-              <span>Graphic Designer</span>
-              <a href="#" title="">プロフィールを表示</a>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="user-profy">
-              <img src="../assets/images/resources/user2.png" alt="" />
-              <h3>John Doe</h3>
-              <span>Graphic Designer</span>
-              <a href="#" title="">プロフィールを表示</a>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="user-profy">
-              <img src="../assets/images/resources/user2.png" alt="" />
-              <h3>John Doe</h3>
-              <span>Graphic Designer</span>
-              <a href="#" title="">プロフィールを表示</a>
+              <img
+                v-if="user.avatar == null"
+                src="../assets/images/resources/user2.png"
+                alt=""
+              />
+              <img v-else :src="user.avatar" width="38px" alt="" />
+
+              <h3>{{ user.name }}</h3>
+              <span>{{ user.experience }}</span>
+              <a :href="'/user-profile?id=' + user.id" title=""
+                >View Profile</a
+              >
             </div>
           </div>
         </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="prev"
-        >
-          <span
-            class="slick-previous slick-arrow"
-            style="display: inline"
-          ></span>
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="next"
-        >
-          <span class="slick-nexti slick-arrow" style="display: inline"></span>
-        </a>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+// import Vue from 'vue'
+import axios from "axios";
+
+// import component1 from 'component1'
+// import component2 from 'component2'
+
+// bat dau phan than cua chuong trinh vuejs
+export default {
+  /***********************************************************************************************************
+   ******************************* Pass data to child component **********************************************
+   **********************************************************************************************************/
+  // props: [variable1, variable2],
+  // components: {component1, component2},
+  data() {
+    /***********************************************************************************************************
+     ******************************* Initialize global variables ***********************************************
+     **********************************************************************************************************/
+    return {
+      msg: "Hello world!",
+      user: [],
+      token: sessionStorage.getItem("token"),
+    };
+  },
+  created() {
+    /***********************************************************************************************************
+     *********************** Initialize data when this component is used. **************************************
+     **********************************************************************************************************/
+    this.mostFollowed();
+  },
+  mounted() {
+    /***********************************************************************************************************
+     ******************** Once created, the interface is displayed and calls mounted. **************************
+     **********************************************************************************************************/
+  },
+  watch: {
+    /***********************************************************************************************************
+     ********************************* Methods change value for a variable *************************************
+     **********************************************************************************************************/
+    msg() {
+      console.log(
+        "When the value of the msg variable changes, this method will be executed."
+      );
+    },
+  },
+  computed: {
+    appendMsg() {
+      return (
+        msg +
+        "Process the value and assign the value to the corresponding variable the var has changed."
+      );
+    },
+  },
+  methods: {
+    /***********************************************************************************************************
+     ******************************* Default functions that handle local data **********************************
+     **********************************************************************************************************/
+
+    /**
+     * Example default function not using param
+     */
+    defaultFunction() {
+      this.msg = "Replace message here!";
+    },
+
+    /**
+     * Example default function using param
+     *
+     * @param int pageNum number of page
+     * @return boolean
+     */
+    defaultFunctionUsingParam(pageNum) {
+      console.log(pageNum);
+      return false;
+    },
+
+    /***********************************************************************************************************
+     ******* Async and await functions for manipulating server-side data through internal API protocols ********
+     **********************************************************************************************************/
+
+    /**
+     * Call API sample
+     */
+    async mostFollowed() {
+      try {
+        const callAPI = await axios.get(
+          "http://localhost/wisesocial_api/public/api/most-followed",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + this.token,
+            },
+          }
+        );
+        if (callAPI.data.code == 200) {
+          this.user = callAPI.data.data;
+        } else {
+          alert("Call API failed, please check again!");
+        }
+        console.log(callAPI.data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+/**
+* Custom local style css
+*/
+</style>
